bc666c542560770668dfd219564bb131049954c5
Benjamin <benseed.bs@gmail.com>
2023-03-07 22:05:31 +0000

    rendered lists and used stateful lists

--- src/components/Expenses/ExpenseItem.jsx
+++ src/components/Expenses/ExpenseItem.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import "./ExpenseItem.css";
 import ExpenseDate from "../Expenses/ExpenseDate";
 import Card from "../UI/Card";
@@ -7,26 +7,20 @@ import Card from "../UI/Card";
 // This acts as a function (all onClick, and 'on'listener props require functions)
 // end all these functions in 'Handeler', e.g clickHandler, eventHandler
 const ExpenseItem = (props) => {
-  const [title, setTitle] = useState(props.title);
-  console.log("expense item evaluated");
-
-  // let title = props.title;
-
-  const clickHandler = () => {
-    setTitle("Updated!");
-    console.log(title);
-  };
-
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">£{props.amount}</div>
       </div>
-      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 };
 
 export default ExpenseItem;
+
+// const clickHandler = () => {
+//   setTitle("Updated!");
+//   console.log(title);
+// };
--- src/components/Expenses/Expenses.jsx
+++ src/components/Expenses/Expenses.jsx
@@ -18,7 +18,15 @@ const Expenses = (props) => {
           selected={filteredYear}
           onChangeFilter={filterChangeHandler}
         />
-        <ExpenseItem
+        {props.expenses.map((expense) => (
+          <ExpenseItem
+            key={expense.id}
+            title={expense.title}
+            amount={expense.amount}
+            date={expense.date}
+          />
+        ))}
+        {/* <ExpenseItem
           title={props.expenses[0].title}
           amount={props.expenses[0].amount}
           date={props.expenses[0].date}
@@ -37,7 +45,7 @@ const Expenses = (props) => {
           title={props.expenses[3].title}
           amount={props.expenses[3].amount}
           date={props.expenses[3].date}
-        />
+        /> */}
       </Card>
     </div>
   );
